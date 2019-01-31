dcde8a6035da0d0783700dbb1a4b495b3c6ea5c2
Orlayhemmy <daminomics@gmail.com>
2019-01-31 20:32:03 +0100

    feat(filter-modal): a user should be able to filter the data displayed - create a re-usable modal - create a filter - add style to the modal

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,36 @@
   <div id='app'>
     <div class='page-head'>
       <span class='page-head__page-title'>Transactions</span>
+      <div class='filter'>
+        <data-filter
+          title='Filter'
+          action-button-text='Filter'
+          :handle-submit='filterData'
+          ref='filter'
+        >
+          <div class='filter-content'>
+            <div v-show='filterBy === "amount"' class='filter-prop'>
+              <p @click='setFilter("date")'>Filter by date</p>
+              <span class='error-message'>{{error.message}}</span>
+              <label>Amount</label>
+              <span class='error-message'>{{error.amount}}</span>
+              <div>
+                <input type='number' v-model='searchCriteria.minAmount' placeholder='Min'>
+                <input type='number' v-model='searchCriteria.maxAmount' placeholder='Max'>
+              </div>
+            </div>
+            <div v-show='filterBy === "date"' class='filter-prop'>
+              <p @click='setFilter("amount")'>Filter by amount</p>
+              <label>Date</label>
+              <span class='error-message'>{{error.date}}</span>
+              <div>
+                <input type='date' v-model='searchCriteria.startDate' placeholder='Start'>
+                <input type='date' v-model='searchCriteria.endDate' placeholder='End'>
+              </div>
+            </div>
+          </div>
+        </data-filter>
+      </div>
     </div>
     <div>
       <data-table :table-head='tableHead'>
@@ -18,15 +48,30 @@
 </template>
 
 <script>
+import Modal from './components/Modal.vue';
 import Table from './components/Table.vue';
 
 export default {
   name: 'app',
   components: {
+    'data-filter': Modal,
     'data-table': Table
   },
   data() {
     return {
+      initialState: {
+        minAmount: process.env.dbUrl,
+        maxAmount: '',
+        startDate: '',
+        endDate: ''
+      },
+      searchCriteria: { ...this.initialState },
+      error: {
+        date: '',
+        amount: '',
+        message: ''
+      },
+      filterBy: 'amount',
       tableHead: [
         {
           title: 'Name',
@@ -51,6 +96,18 @@ export default {
     tableData() {
       return this.$store.getters.tableData;
     },
+  },
+  methods: {
+    setFilter(value) {
+      this.filterBy = value;
+      this.searchCriteria = { ...this.initialState };
+      this.error = {
+        date: '',
+        amount: '',
+        message: '',
+      }
+    },
+    filterData() {}
   },
   created() {
     this.$store.dispatch('getTransactions');
