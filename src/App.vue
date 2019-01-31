d53eceb2713d4d908cfe2a1b87879ecd80a66259
Orlayhemmy <daminomics@gmail.com>
2019-01-31 20:52:56 +0100

    feat(filter-data): user should be able to filter and sort data in the table - filter data based on amount and date - sort data based on amount and date - edit the description of a transaction

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,23 @@
       <data-table :table-head='tableHead'>
         <tr v-for='entry in tableData' v-bind:key='entry[".key"]'>
           <td>{{entry.name}}</td>
-          <td>{{entry.description}}</td>
+          <td>
+            <div class='dt_description' v-show='getDataId !== entry.dataKey'>
+              {{entry.description}}
+              <img
+                src='./assets/images/edit.svg'
+                alt='edit-icon'
+                @click='toggleInputBox(entry)'
+              >
+            </div>
+            <div class='dt_description_input' v-show='getDataId === entry.dataKey'>
+              <input type='text' v-model='getDescription'>
+              <div class='action-buttons'>
+                <span @click='updateDescription'>&#x2713;</span>
+                <span @click='toggleInputBox'>&#x2715;</span>
+              </div>
+            </div>
+          </td>
           <td>{{entry.amount}}</td>
           <td>{{entry.date}}</td>
           <td>&#x1f5d1;</td>
@@ -96,8 +112,33 @@ export default {
     tableData() {
       return this.$store.getters.tableData;
     },
+    getDescription: {
+      get() {
+        return this.$store.getters.getDescription;
+      },
+      set(value) {
+        const payload = {
+          content: value,
+          id: this.getDataId,
+        };
+        return this.$store.commit('updateStateProp', payload);
+      },
+    },
+    getDataId() {
+      return this.$store.getters.getDataId;
+    },
   },
   methods: {
+    toggleInputBox(data = null) {
+      const payload = {
+        id: data ? data.dataKey : null,
+        content: data ? data.description : null
+      };
+      this.$store.dispatch('toggleInputBox', payload);
+    },
+    updateDescription() {
+      this.$store.dispatch('updateDescriptionAction');
+    },
     setFilter(value) {
       this.filterBy = value;
       this.searchCriteria = { ...this.initialState };
@@ -107,7 +148,36 @@ export default {
         message: '',
       }
     },
-    filterData() {}
+    filterData() {
+      let filterParams = {};
+      const { startDate, endDate, minAmount, maxAmount } = this.searchCriteria;
+      const validInput = this.validateUserInputs();
+      if (validInput) {
+        this.$refs.filter.toggleModal();
+        minAmount && (filterParams.minAmount = Number(minAmount));
+        maxAmount && (filterParams.maxAmount = Number(maxAmount));
+        startDate && (filterParams.startDate = startDate);
+        endDate && (filterParams.endDate = endDate);
+        return this.$store.dispatch('getTransactions', {
+          filterParams,
+          filterBy: this.filterBy,
+        });
+      }
+    },
+    validateUserInputs() {
+      const { startDate, endDate, minAmount, maxAmount } = this.searchCriteria;
+      if (!startDate && !endDate && !minAmount && !maxAmount) {
+        this.error.message = 'No data inputted';
+        return;
+      } else if (maxAmount && (minAmount > maxAmount || !minAmount)) {
+        this.error.amount = 'Maximum amount should greater than minimum amount';
+        return;
+      } else if (endDate && startDate > endDate) {
+        this.error.date = 'End date should be greater than start date';
+        return;
+      }
+      return true;
+    },
   },
   created() {
     this.$store.dispatch('getTransactions');
--- src/store.js
+++ src/store.js
@@ -7,15 +7,40 @@ Vue.use(db);
 
 export const state = {
   tableData: [],
+  dataId: '',
+  description: '',
+  error: '',
 };
 
 export const getters = {
   tableData(state) {
     return state.tableData;
   },
+  getDescription(state) {
+    return state.description;
+  },
+  getDataId(state) {
+    return state.dataId;
+  },
 };
 
 export const mutations = {
+  updateEntryDescription(state) {
+    state.tableData = state.tableData.map(entry =>
+      (entry.dataKey === state.dataId
+        ? { ...entry, description: state.description }
+        : entry
+      ));
+    state.dataId = null;
+  },
+  updateStateProp(state, payload) {
+    state.description = payload.content;
+    state.dataId = payload.id ? payload.id : null;
+  },
+  sortTableOptions(state, payload) {
+    const key = payload.toLowerCase();
+    state.tableData = state.tableData.sort((a, b) => (a[`${key}`] > b[`${key}`] ? 1 : b[`${key}`] > a[`${key}`] ? -1 : 0));
+  },
   populateTable(state, payload = null) {
     payload && payload.forEach((doc) => {
       const obj = {
@@ -27,11 +52,37 @@ export const mutations = {
       state.tableData.push(obj);
     });
   },
+  displayError(state, payload) {
+    state.error = payload;
+  },
 };
 
 export const actions = {
-  getTransactions(context) {
-     db.collection('transactions').onSnapshot((snapshot) => {
+  sortTableData(context, payload) {
+    context.commit('sortTableOptions', payload);
+  },
+  toggleInputBox(context, payload) {
+    context.commit('updateStateProp', payload);
+  },
+  updateDescriptionAction(context) {
+    return db.collection('transactions').doc(context.state.dataId).update({
+      description: context.state.description,
+    }).then(() => context.commit('updateEntryDescription'))
+      .catch(() => context.commit('displayError', 'Data update unsuccessful! try again'));
+  },
+  getTransactions(context, payload = null) {
+    const { filterParams, filterBy } = payload || {};
+    let query;
+
+    if (payload) {
+      query = Object.values(filterParams)[0] > Object.values(filterParams)[1]
+        ? db.collection('transactions').where(filterBy, '>', Object.values(filterParams)[1]).where(filterBy, '<', Object.values(filterParams)[0])
+        : db.collection('transactions').where(filterBy, '>', Object.values(filterParams)[0]).where(filterBy, '<', Object.values(filterParams)[1])
+    } else {
+      query = db.collection('transactions');
+    }
+
+    query.onSnapshot((snapshot) => {
       context.state.tableData = [];
       context.commit('populateTable', snapshot.docs);
     });
